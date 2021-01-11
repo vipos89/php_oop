4ebfd378ffec6e8ff6c404f3b8c2497e0c067288
vipos89 <webmaster.kobrin@gmail.com>
2021-01-11 20:11:06 +0300

    models

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
 
-require_once './vendor/autoload.php';
-
-use App\Router;
-
-define('base_path', __DIR__);
-define('DS', DIRECTORY_SEPARATOR);
-
-$router = new Router();
-$router->getRoute();
+echo "<h1>Hello world</h1>";
+//require_once './vendor/autoload.php';
+//
+//use App\Router;
+//
+//define('base_path', __DIR__);
+//define('DS', DIRECTORY_SEPARATOR);
+//
+//$router = new Router();
+//$router->getRoute();
 
 
 
